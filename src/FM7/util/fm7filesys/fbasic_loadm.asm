1b7c71bb5bb34f041b2e6657c87b72d1928bbd60
CaptainYS <soji@andrew.cmu.edu>
2019-09-18 11:35:15 -0400

    Much cleaner by adding one flag.

--- src/FM7/util/fm7filesys/fbasic_loadm.asm
+++ src/FM7/util/fm7filesys/fbasic_loadm.asm
@@ -23,17 +23,21 @@ FSYS_FILE_LOADM_FILE_INFO		EQU		FSYS_FILE_LOADM_STACK_SIZE+1
 FSYS_FILE_LOADM_FAT_BUFFER		EQU		FSYS_FILE_LOADM_STACK_SIZE+3
 FSYS_FILE_LOADM_SECTOR_BUFFER	EQU		FSYS_FILE_LOADM_STACK_SIZE+5
 
+FSYS_FILE_LOADM_SECTOR_LOADED	EQU		9
+FSYS_FILE_LOADM_BYTE_LEFT		EQU		10
+FSYS_FILE_LOADM_EXEC_ADDR		EQU		18
+
 FSYS_FILE_LOADM			PSHS	U,Y,X,B
 						;	[5+FSYS_FILE_LOADM_STACK_SIZE,S]	U
 						;	[3+FSYS_FILE_LOADM_STACK_SIZE,S]	Y
 						;	[1+FSYS_FILE_LOADM_STACK_SIZE,S]	X
 						;	[FSYS_FILE_LOADM_STACK_SIZE,S]	B
 
-						;	[15,S]	5 bytes	Last 5 bytes
+						;	[15,S]	5 bytes	Last 5 bytes  [18,S] 2-bytes EXEC address.
 						;	[14,S]	1 byte	0:Reading main body   FF:Reading last 5 bytes
 						;	[12,S]	2 bytes	Current Data Pointer
 						;	[10,S]	2 bytes	Bytes left	
-						;	[9,S]	1 byte	# Clusters
+						;	[9,S]	1 byte	Sector_is_loaded flag
 						;	[8,S]	1 byte	Bytes into sector
 						;	[7,S]	1 byte	Drive
 						;	[6,S]	1 byte	Current Side
@@ -95,46 +99,110 @@ FSYS_FILE_LOADM			PSHS	U,Y,X,B
 						STD		12,S	; Current Data Pointer
 						STD		8,X		; Top Address
 						LDD		1,U
-						STD		10,S	; Bytes left
+						STD		FSYS_FILE_LOADM_BYTE_LEFT,S
 						STD		6,X		; Size in bytes
 
 						; Already used 5 bytes into the sector data.
 						LDA		#5
 						STA		8,S
+						STA		FSYS_FILE_LOADM_SECTOR_LOADED,S	; Non-Zero means sector is loaded.
 
 						CLR		14,S	; Reading the main body
 
-						; While(0<word ptr [10,S])
+
+						;while(0<bytes_left)
+						;{
+						;	if(0==sector_is_loaded)
+						;	{
+						;		LoadSector();
+						;		sector_is_loade=255;
+						;	}
+						;	while(0<bytes_left && 0!=(bytes_into_sector&255))
+						;	{
+						;		CopyOneByte();
+						;	}
+						;	sector_is_loade=0;
+						;	if(0==bytes_left)
+						;	{
+						;		break;
+						;	}
+						;	for(;;)
+						;	{
+						;		MoveToNextSector();
+						;		if(true==EndOfCluster())
+						;		{
+						;			break;
+						;		}
+						;		if(bytes_left<256)
+						;		{
+						;			goto NEXT_BYTE;
+						;		}
+						;		ReadSectorDirectlyToTheDestination();
+						;		bytes_left-=256;
+						;	}
+						;	for(;;)
+						;	{
+						;		MoveToNextCluster();
+						;		if(bytes_left<0x800)
+						;		{
+						;			break;
+						;		}
+						;		ReadClusterDirectlyToTheDestination();
+						;		bytes_left-=0x800;
+						;	}
+						;
+						;NEXT_BYTE:
+						;	;
+						;}
+
+
+						; While(0<word ptr [BYTES_LEFT,S])
 FSYS_FILE_LOADM_BYTE_LEFT_LOOP
+						; FSYS_FILE_LOADM_SECTOR_LOADED, is needed for:
+						; (1) Avoiding to read the first sector twice.
+						; (2) When bytes-left and bytes-into-sector become zero together at the end of
+						;     loading the main body (before reading the trailing 5 bytes), the loop 
+						;     for reading the trailing 5 bytes starts with bytes-into-sector==0,
+						;     which appears that a whole sector 256 bytes is available, but actually
+						;     the sector was exhausted.
+						LDU		FSYS_FILE_LOADM_SECTOR_BUFFER,S
+
+						LDA		FSYS_FILE_LOADM_SECTOR_LOADED,S
+						BNE		FSYS_FILE_LOADM_LOADED_SECTOR
+
+						LDD		4,S
+						LDX		6,S
+						LBSR	FSYS_BIOS_READSECTOR
+						COM		FSYS_FILE_LOADM_SECTOR_LOADED,S	; Non-Zero means sector is loaded.
+FSYS_FILE_LOADM_LOADED_SECTOR
 
 						LDX		12,S	; Current data pointer
 						LDB		8,S		; Bytes into the sector (#5 only for the first sector)
 						CLRA
 						LEAY	D,U		; 
 
-
 FSYS_FILE_LOADM_BYTE_LEFT_INSIDE_LOOP
-						LDD		10,S
+						LDD		FSYS_FILE_LOADM_BYTE_LEFT,S
 						BEQ		FSYS_FILE_LOADM_BYTE_LEFT_LOOP_EXIT
 						SUBD	#1
-						STD		10,S
+						STD		FSYS_FILE_LOADM_BYTE_LEFT,S
 
 						LDA		,Y+
 						STA		,X+
+						STX		12,S
 
 						INC		8,S
 						BNE		FSYS_FILE_LOADM_BYTE_LEFT_INSIDE_LOOP
 
-						STX		12,S
-
-						LDD		10,S
-						BEQ		FSYS_FILE_LOADM_BYTE_LEFT_LOOP_EXIT
-						; I have a feeling that exception should be handled here.
-						; Rather than at the end (with backward GOTO.)
-
-
-FSYS_FILE_LOADM_BYTE_LEFT_LOOP_EXCEPTION_ENTRY
 						; Used up a sector.
+
+						CLR		FSYS_FILE_LOADM_SECTOR_LOADED,S	; Zero means sector buffer is exhausted.
+
+						LDD		FSYS_FILE_LOADM_BYTE_LEFT,S
+						BEQ		FSYS_FILE_LOADM_BYTE_LEFT_LOOP_EXIT
+
+
+
 						; while(0x100<=bytes_left && not the last sector of the cluster)
 						LDU		12,S	; Current Data Pointer
 
@@ -142,10 +210,10 @@ FSYS_FILE_LOADM_SECTOR_LOOP
 						LDB		5,S
 						INC		5,S
 						BITB	#7
-						BEQ		FSYS_FILE_LOADM_END_OF_CLUSTER
+						BEQ		FSYS_FILE_LOADM_NEXT_CLUSTER
 
-						LDA		10,S	; Higher-byte of the bytes left
-						BEQ		FSYS_FILE_LOADM_NOT_END_OF_CLUSTER
+						LDA		FSYS_FILE_LOADM_BYTE_LEFT,S	; Higher-byte of the bytes left
+						BEQ		FSYS_FILE_LOADM_BYTE_LEFT_LOOP	; Fall back to byte-by-byte reading.
 
 						LDD		4,S
 						LDX		6,S
@@ -153,12 +221,11 @@ FSYS_FILE_LOADM_SECTOR_LOOP
 
 						LEAU	$100,U
 						STU		12,S
-						DEC		10,S
+						DEC		FSYS_FILE_LOADM_BYTE_LEFT,S
 						BRA		FSYS_FILE_LOADM_SECTOR_LOOP
+FSYS_FILE_LOADM_NEXT_CLUSTER
 
 
-
-FSYS_FILE_LOADM_END_OF_CLUSTER
 
 						; while(0x800<bytes_left)
 FSYS_FILE_LOADM_BURST_READ_MID_CLUSTER_LOOP
@@ -169,10 +236,10 @@ FSYS_FILE_LOADM_BURST_READ_MID_CLUSTER_LOOP
 						BCS		FSYS_FILE_LOADM_DEVICE_IO_ERROR
 
 						; Mid-cluster?  If so can I burst-read?
-						LDA		10,S	; Higher-byte of the bytes left
+						LDA		FSYS_FILE_LOADM_BYTE_LEFT,S	; Higher-byte of the bytes left
 						SUBA	#8
-						BCS		FSYS_FILE_LOADM_CLUSTER_BURST_READ_END	; Less than $800 bytes left.
-						STA		10,S
+						BCS		FSYS_FILE_LOADM_BYTE_LEFT_LOOP	; Less than $800 bytes left.
+						STA		FSYS_FILE_LOADM_BYTE_LEFT,S
 
 						LDB		,Y	; 	Cluster
 						LDA		7,S	; 	Drive
@@ -181,36 +248,20 @@ FSYS_FILE_LOADM_BURST_READ_MID_CLUSTER_LOOP
 						STU		12,S
 
 						BRA		FSYS_FILE_LOADM_BURST_READ_MID_CLUSTER_LOOP
-FSYS_FILE_LOADM_CLUSTER_BURST_READ_END
-
-
-
-FSYS_FILE_LOADM_NOT_END_OF_CLUSTER
-						LDU		FSYS_FILE_LOADM_SECTOR_BUFFER,S
-						LDD		4,S
-						LDX		6,S
-						LBSR	FSYS_BIOS_READSECTOR
-
-						BRA		FSYS_FILE_LOADM_BYTE_LEFT_LOOP
 
 
 
 FSYS_FILE_LOADM_BYTE_LEFT_LOOP_EXIT
 						LEAX	15,S
 						STX		12,S
-						LDB		#5		; Word Ptr [10,S] is zero at this point.
+						LDB		#5		; Word Ptr [FSYS_FILE_LOADM_BYTE_LEFT,S] is zero at this point.
 						STB		11,S	; Only needs lower byte to be set.
 
 						COM		14,S
 						BEQ		FSYS_FILE_LOADM_FINISHED_LAST_5_BYTE
 
-						; End of data and end of sector may come together.
-						; In that case, the loop exits with [8,S] (bytes into sector)=0
-						; [8,S]==0 in the previous loop means there are 256 bytes of data to look at.
-						; But, at this point, [8,S]==0 means the sector buffer is empty.
-						TST		8,S
-						LBEQ	FSYS_FILE_LOADM_BYTE_LEFT_LOOP_EXCEPTION_ENTRY
 						LBRA	FSYS_FILE_LOADM_BYTE_LEFT_LOOP
+
 
 
 FSYS_FILE_LOADM_FINISHED_LAST_5_BYTE
@@ -219,7 +270,7 @@ FSYS_FILE_LOADM_FINISHED_LAST_5_BYTE
 						; Last 5 bytes
 						; +0	3-bytes	$FF,$00,$00
 						; +3	2-bytes	EXEC ADDR
-						LDD		18,S
+						LDD		FSYS_FILE_LOADM_EXEC_ADDR,S
 						ADDD	2,X
 						STD		10,X
 
