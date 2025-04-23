d917a748c73ce1bf05ab32eaab2f722a9e3df956
Soji Yamakawa <soji@andrew.cmu.edu>
2025-04-23 11:02:44 -0400

    Fixing build.

--- src/ysclass/src/yshash.h
+++ src/ysclass/src/yshash.h
@@ -1879,7 +1879,7 @@ public:
 				return true;
 			}
 			else if(this->ended!=YSTRUE && from.ended!=YSTRUE &&
-			        this->hashIdx==from.hashIdx && this->arrayIdx==from.arrayIdx)
+			        this->hd.hashIdx==from.hd.hashIdx && this->hd.arrayIdx==from.hd.arrayIdx)
 			{
 				return true;
 			}
